ef341a48b28269f9fae67cca26e898579786727c
root <root@localhost.localdomain>
2022-07-18 19:34:52 +0300

    add cards

---
+++ src/components/UI/card.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+  <div class="card">
+    <span class="card-name">{{ name }}</span>
+    <span class="card-title">{{ title }}</span>
+    <div class="card-body">13</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="css" scoped></style>
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,11 +1,35 @@
 <template lang="html">
   <p>hello vew</p>
+  <div class="card_wrapper" v-for="item in items" :key="item.id">
+    <Card :name="item.lvl + ' lvl'" :title="item.title" />
+  </div>
 </template>
 
 <script>
+import Card from "@/components/UI/card";
 export default {
-}
+  components: {
+    Card,
+  },
+  data() {
+    return {
+      items: [
+        {
+          id: 1,
+          title: "Archer",
+          descr: "archer archer archer archer",
+          lvl: 4,
+        },
+        {
+          id: 2,
+          title: "Wizard",
+          descr: "Wizard Wizard Wizard Wizard",
+          lvl: 5,
+        },
+      ],
+    };
+  },
+};
 </script>
 
-<style lang="css" scoped>
-</style>
+<style lang="css" scoped></style>
